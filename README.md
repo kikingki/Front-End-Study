c59958fa1704918aeb3c19187544a973d38181c8
kikingki <mongbu54@gmail.com>
2021-01-10 21:25:05 +0900

    Sign up

---
+++ README.md
@@ -0,0 +1,25 @@
+# Front End Study✍
+#### 20.12.23
+  + ##### Flex & Grid
+    + vw, vh 단위를 알게 됐다. viewport에 맞춰 상대적인 크기를 반환하므로 반응형 웹사이트 제작에 편리
+    + flex의 주축과 부축에 따른 정렬 방향, 줄바꿈 설정 등 flex의 속성을 공부했다.
+    + fr은 grid를 분할하여 영역을 차지
+    <br/>
+#### 20.12.28
+  + ##### JS
+    + JavaScript는 interactive한 개발을 위해 필요하다.
+    + querySelector로 요소를 불러와 변수를 저장
+    + 화살표 함수를 사용해 숫자를 증감시키는 increase, decrease 선언
+    + classList의 add, remove, toggle 메서드를 이용해 클래스를 조작하는 방법을 배웠다.
+    + transition으로 애니메이션 효과를 넣음
+    <br/>
+ #### 20.1.4
+  + ##### Todo
+    + localStorage를 사용해 웹페이지의 세션이 끝나도 데이터가 지워지지 않도록 한다.
+    + todo를 생성할 때 id와 value를 딕셔너리로 저장하여 삭제도 가능하도록
+    + filter는 반환 값이 true인 것만 모아 리스트로 반환한다.
+#### 20.1.5
+  + #### Responsive Web
+    + * selector는 모든 객체에 적용
+    + flex 속성을 사용해 반응형 navbar와 todo 리스트를 만들었다.
+    + @media를 사용해 width가 1000px 이하일 경우 todo 리스트를 한줄로 정렬하고 배경색을 바꿈
---
+++ signup/signup.js
@@ -0,0 +1,69 @@
+
+
+const form = document.querySelector('.js-form'),
+    username = form.querySelector('.js-username'),
+    email = form.querySelector('.js-email'),
+    password = form.querySelector('.js-password'),
+    check_password = form.querySelector('.js-check_password');
+
+
+form.addEventListener("submit", (e)=> {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+const setErrorMessage = (input, message) => {
+    const formElement = input.parentElement;
+    const small = formElement.querySelector('.js-small');
+
+    small.innerText = message;
+    formElement.classList.add('error');
+}
+
+const setSucess = (input) => {
+    const formElement = input.parentElement;
+    formElement.classList.remove('error'); 
+    formElement.classList.add('success'); 
+}
+
+const checkInputs = () => {
+    //trim() 메서드는 양쪽의 공백을 제거
+    const n = username.value.trim();
+    const e = email.value.trim();
+    const p = password.value.trim();
+    const cp = check_password.value.trim();
+
+    const regEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if(n === '') {
+        setErrorMessage(username, "Username can't be blank");
+    } else {
+        setSucess(username);
+    }
+
+    if(e === ''){
+        setErrorMessage(email, "Email can't be blank");
+    } else if(!regEmail.test(e)) {
+        setErrorMessage(email, "Email is not valid" );
+    } else {
+        setSucess(email);
+    }
+
+    if(p === ''){
+        setErrorMessage(password, "Password can't be blank");
+    } else if (p.length < 6){
+        setErrorMessage(password, "Password must be at least 6 characters");
+    } else {
+        setSucess(password);
+    }
+
+    if(p === ''){
+        setErrorMessage(check_password, "Confirm password can't be blank");
+    } else if (p!==cp){
+        setErrorMessage(check_password, "Passwords do not match");
+    } else {
+        setSucess(password);
+    }
+}
+
