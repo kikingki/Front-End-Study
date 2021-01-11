96db00308577987be282f1ef2428fe8c5117e7e3
kikingki <mongbu54@gmail.com>
2021-01-11 19:19:34 +0900

    edit Sign up & Update README.md

--- README.md
+++ README.md
@@ -4,7 +4,6 @@
     + vw, vh 단위를 알게 됐다. viewport에 맞춰 상대적인 크기를 반환하므로 반응형 웹사이트 제작에 편리
     + flex의 주축과 부축에 따른 정렬 방향, 줄바꿈 설정 등 flex의 속성을 공부했다.
     + fr은 grid를 분할하여 영역을 차지
-    <br/>
 #### 20.12.28
   + ##### JS
     + JavaScript는 interactive한 개발을 위해 필요하다.
@@ -12,7 +11,6 @@
     + 화살표 함수를 사용해 숫자를 증감시키는 increase, decrease 선언
     + classList의 add, remove, toggle 메서드를 이용해 클래스를 조작하는 방법을 배웠다.
     + transition으로 애니메이션 효과를 넣음
-    <br/>
  #### 20.1.4
   + ##### Todo
     + localStorage를 사용해 웹페이지의 세션이 끝나도 데이터가 지워지지 않도록 한다.
@@ -20,6 +18,9 @@
     + filter는 반환 값이 true인 것만 모아 리스트로 반환한다.
 #### 20.1.5
   + #### Responsive Web
-    + * selector는 모든 객체에 적용
     + flex 속성을 사용해 반응형 navbar와 todo 리스트를 만들었다.
     + @media를 사용해 width가 1000px 이하일 경우 todo 리스트를 한줄로 정렬하고 배경색을 바꿈
+#### 20.1.10
++ #### Sign up
+  + javascript를 다룬지 오랜만이어서 구글링을 아주 많이 했다ㅎㅎ 확실히 프로젝트를 진행해보니 배웠던 개념을 더 정확하게 알 수 있었고 어떻게 응용하면 되는 지 알게 됐다. 이벤트가 전달되면 checkInputs() 메서드로 유효성을 검사했다. setErrorMessage() 메서드를 만들어 값이 유효하지 않으면  classList로 변수명에 "error"를 추가해 error message를 출력했다. 
+  + css적인 부분은 flex를 사용해 반응형으로 만들었고, 일정 width 이하면 background-color를 바꿨다. 그리고, 애니메이션으로 header가 공이 튀는 듯한 모션을 넣었다.
--- signup/signup.js
+++ signup/signup.js
@@ -4,36 +4,36 @@ const form = document.querySelector('.js-form'),
     username = form.querySelector('.js-username'),
     email = form.querySelector('.js-email'),
     password = form.querySelector('.js-password'),
-    check_password = form.querySelector('.js-check_password');
+    checkPwd = form.querySelector('.js-check_password');
 
 
-form.addEventListener("submit", (e)=> {
-    e.preventDefault();
+const SUCCESS = 'success';
+const ERROR = 'error';
 
-    checkInputs();
-});
-
+// error message 출력 함수
 const setErrorMessage = (input, message) => {
-    const formElement = input.parentElement;
+    const formElement = input.parentElement;    // input의 부모노드를 가져옴
     const small = formElement.querySelector('.js-small');
 
-    small.innerText = message;
-    formElement.classList.add('error');
+    small.innerText = message;              // smail 안의 text를 매개변수로 받은 message로 변경
+    formElement.classList.add(ERROR);     //classList로 변수명에 error 추가
 }
 
+// 변수명에 success를 추가하는 함수
 const setSucess = (input) => {
     const formElement = input.parentElement;
-    formElement.classList.remove('error'); 
-    formElement.classList.add('success'); 
+    formElement.classList.remove(ERROR); 
+    formElement.classList.add(SUCCESS); 
 }
 
 const checkInputs = () => {
-    //trim() 메서드는 양쪽의 공백을 제거
+    // trim() 메서드는 양쪽의 공백을 제거
     const n = username.value.trim();
     const e = email.value.trim();
     const p = password.value.trim();
-    const cp = check_password.value.trim();
+    const cp = checkPwd.value.trim();
 
+    // 정규식으로 email 유효성 검사
     const regEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if(n === '') {
@@ -44,7 +44,7 @@ const checkInputs = () => {
 
     if(e === ''){
         setErrorMessage(email, "Email can't be blank");
-    } else if(!regEmail.test(e)) {
+    } else if(!regEmail.test(e)) {      // test() 메서드는 주어진 문자열이 정규 표현식을 만족하는지 판별하고, 그 여부를 true 또는 false로 반환
         setErrorMessage(email, "Email is not valid" );
     } else {
         setSucess(email);
@@ -59,11 +59,20 @@ const checkInputs = () => {
     }
 
     if(p === ''){
-        setErrorMessage(check_password, "Confirm password can't be blank");
+        setErrorMessage(checkPwd, "Confirm password can't be blank");
     } else if (p!==cp){
-        setErrorMessage(check_password, "Passwords do not match");
+        setErrorMessage(checkPwd, "Passwords do not match");
     } else {
-        setSucess(check_password);
+        setSucess(checkPwd);
     }
 }
 
+const init = () => {
+    form.addEventListener("submit", (e)=> {
+        e.preventDefault();
+    
+        checkInputs();
+    })
+}
+
+init();
